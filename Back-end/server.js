5b8379efc72e87c375801ebfc0a361dac8d1ab08
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2024-06-11 00:51:41 -0300

    Project  full stack in back-end

---
+++ Back-end/server.js
@@ -0,0 +1,73 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+// Metodos
+
+// 1 - Criar Usuarios
+app.post("/usuarios", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json(req.body);
+});
+
+// 2 - Listar Usuarios
+// app.get("/usuarios", async (req, res) => {
+//   const users = await prisma.user.findMany();
+//   res.status(200).json(users);
+// });
+
+// 2 - Listar Usuarios com filtro
+app.get("/usuarios", async (req, res) => {
+  let users = [];
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.name,
+        age: req.query.age,
+      },
+    });
+  } else {
+    users = await prisma.user.findMany();
+  }
+  res.status(200).json(users);
+});
+
+// 3 - Editar Usuarios
+app.put("/usuarios/:id", async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json(req.body);
+});
+
+// 4 - Deletar Usuarios
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({ message: "Usuario deletado com Sucesso!" });
+});
+
+app.listen(4000);
+
+// marcelo  -  eZZBrRg9tkF0Rqnl
---
+++ README.md
@@ -0,0 +1,43 @@
+## Getting Started Back-end
+
+Run the development server:
+
+npm i 
+
+
+Open [http://localhost:4000/usuarios/](http://localhost:4000/usuarios/) with your browser to see the result = API ok.
+
+
+## Libs
+
+- [Node Documentation](https://node.com.br) - Principal ferramenta.
+- [Prisma](https://www.prisma.io/docs/getting-started) - ORM Prisma.
+
+
+## Comandos Utilizados
+
+* npm init -y
+* npm i express
+* node --watch server.js
+* npm i prisma --save-dev
+* npx prisma init
+* npx prisma db push
+* npm install @prisma/client
+* npx prisma studio
+
+
+## Getting Started Front-end
+
+
+Open []() .
+
+
+## Libs
+
+- [](https://) - .
+- [](https://) - .
+
+
+## Comandos Utilizados
+
+
