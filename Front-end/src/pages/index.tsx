2eaf05b2822fc9edfb42169c3697521d2d059d91
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2024-06-13 11:26:15 -0300

    Iniciando front-end

---
+++ Front-end/src/pages/index.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import "./style.css";
+
+function Home() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>Olá rectdasdasd</div>
+    </>
+  );
+}
+
+export default Home;
