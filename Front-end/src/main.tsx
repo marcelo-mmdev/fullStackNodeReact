6b5e716b2c9861d7907fd223975e4a37cbc98649
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2024-06-19 10:59:10 -0300

    integracao com backend instalacao e configuracao do axios

--- Front-end/src/main.tsx
+++ Front-end/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './pages/index.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/index.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ Front-end/src/pages/index.jsx
@@ -0,0 +1,84 @@
+// import { useState } from 'react'
+
+import "./style.css";
+import Trash from "../assets/trash.svg";
+import api from "../services/api";
+import { useEffect, useRef, useState } from "react";
+
+function Home() {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  async function getUsers() {
+    const usersApi = await api.get("/usuarios");
+    setUsers(usersApi.data);
+    console.log(users);
+  }
+
+  async function createUsers() {
+    await api.post("/usuarios", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value,
+    });
+    getUsers()
+  }
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`);
+    getUsers()
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <form>
+          <h1>Cadastro de Usuários</h1>
+          <input placeholder="Nome" name="name" type="text" ref={inputName} />
+          <input
+            placeholder="Idade"
+            name="idade"
+            type="text"
+            ref={inputAge}
+          />
+          <input
+            placeholder="E-mail"
+            name="email"
+            type="email"
+            ref={inputEmail}
+          />
+          <button type="button" onClick={createUsers}>
+            Cadastra
+          </button>
+        </form>
+
+        {users.map((user) => (
+          <div key={user.id} className="card">
+            <div>
+              <p>
+                Nome: <span>{user.name}</span>{" "}
+              </p>
+              <p>
+                Idade: <span>{user.age}</span>{" "}
+              </p>
+              <p>
+                Email: <span>{user.email}</span>{" "}
+              </p>
+            </div>
+            <button onClick={() => deleteUsers(user.id)} >
+              <img src={Trash} alt="Delete" />
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Home;
